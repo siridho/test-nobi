1d7ad81bcce91714b9c0ba0e100f076159b62d60
siridho <siridho@gmail.com>
2022-03-29 21:54:33 +0700

    fix: auth error message

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
    try {
       const {
          headers: { authorization },
@@ -24,5 +24,37 @@ module.exports = (req, res, next) => {
          status: 'error',
          message: 'Unauthorized',
       })
+   }
+
+   try {
+      const { authorization } = req.headers
+
+      if (!authorization) {
+         throw new ErrorHandler(401, 'User is not unathorized')
+      }
+
+      if (!authorization.startsWith('Bearer')) {
+         throw new ErrorHandler(401, 'User is not unathorized')
+      }
+
+      const split = authorization.split('Bearer ')
+      if (split.length !== 2) {
+         throw new ErrorHandler(401, 'User is not unathorized')
+      }
+
+      const token = authorization.split(' ')[1]
+      console.log(token)
+      const decodedToken = jwt.verify(token, process.env.SECRET)
+      const { id } = decodedToken
+      res.setHeader('email', decodedToken.email)
+      if (!id) {
+         throw new ErrorHandler(401, 'User is not unathorized')
+      } else {
+         next()
+      }
+      next()
+   } catch (error) {
+      console.log('END')
+      next(error)
    }
 }
